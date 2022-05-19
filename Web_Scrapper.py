7bf440be8f8caaf2ce836831638a7b6f0d70827a
mebokvinikadze <74296490+mebokvinikadze@users.noreply.github.com>
2022-05-20 01:20:14 +0400

    Update Web_Scrapper.py

--- Web_Scrapper.py
+++ Web_Scrapper.py
@@ -13,79 +13,73 @@ curs = mydb.cursor()
 
 while True:
     try:
-        city = input("Enter City(use Georgian font): ""\n")
-        days_ = input("Enter how many days forecast you want(5,10,15,25): ""\n")
-        curs.execute(f"DROP TABLE IF EXISTS {city}")
-        curs.execute(f"CREATE TABLE {city}(WEATHER VARCHAR(255),WEEKDAY VARCHAR(255),DAY VARCHAR(255))")
+        city = input("\n" "Enter City(use Georgian font): " "\n")  
+        days_ = int(input("\n" "Enter how many days forecast you want(5,10,15,25): " "\n")) 
         URL = (f"https://amindi.ge/ka/city/{city}/?d={days_}")
         req = requests.get(URL)
         soup = BeautifulSoup(req.content, 'html5lib')
+        cities = soup.find('div', class_='dropdown-menu').findAll('ul')[0].findAll('li') + \
+                    soup.find('div', class_='dropdown-menu').findAll('ul')[1].findAll('li')               
+        day = soup.findAll('p', class_='day')
+
         weather = soup.findAll('div', class_='degrees')
         weekdays = soup.findAll('div', class_='weekDay')
-        day = soup.findAll('p',class_='day')
-        daycount = []
 
-        if city == "":
-            pass
-        elif days_ == "":
-            pass
+        citieslist = []      
+
+        for i in cities:
+            varcities = i.find("a").text.rstrip(i.find("a").find("span").text)
+            citieslist.append(varcities)
+
+  
+        if city not in citieslist:
+            print("Wrong City Name!")
+            break
+
+        curs.execute(f"DROP TABLE IF EXISTS {city}")
+        curs.execute(f"CREATE TABLE {city}(WEATHER VARCHAR(255),WEEKDAY VARCHAR(255),DATE VARCHAR(255))")
+
+        daycount = []
+        daytemperature = []
+        nighttemperature = []
+
+        if days_ == "":
+            break
 
         for i in day:
             daycount.append(i.text)
 
-        daytemperature = []
         for i in weather:
             daytemperature.append(i.span.text)
 
         daytemperature.pop(0)
-
-        nighttemperature = []
+ 
         for div in weather:
             span = div.find_next('span').find_next('span')
             nighttemperature.append(span.text)
+            
         nighttemperature.pop(0)
         weekdaycount = []
         for div in weekdays:
-            weekdaycount.append(div.text)
-
-
-        if days_ == "5":    
-            print("ტემპერატურა შემდეგი 5 დღის განმავლობაში")
+            weekdaycount.append(div.text)    
+     
+        #checking if entered days are correct
+        if days_ == 5 or days_ == 10 or days_ == 15 or days_ == 25:
+            print("\n" f"({city}) - ტემპერატურა შემდეგი {days_} დღის განმავლობაში" "\n")
             for a,b,c,d in zip(daytemperature,nighttemperature,weekdaycount,daycount):
                 print(f"{d} - {c} - {a}° - {b}°")
-                insert = f"INSERT INTO {city}(WEATHER,WEEKDAY,DAY) values(%s,%s,%s)"
+                insert = f"INSERT INTO {city}(WEATHER,WEEKDAY,DATE) values(%s,%s,%s)"
                 val = ((f"{a}°-{b}°"),c,d)
                 curs.execute(insert,val)
 
-        elif days_ == "10":
-            print("ტემპერატურა შემდეგი 10 დღის განმავლობაში")
-            for a,b,c,d in zip(daycount,weekdaycount,daytemperature,nighttemperature):
-                print(f"{d} - {c} - {a}° - {b}°")
-                insert = f"INSERT INTO {city}(WEATHER,WEEKDAY,DAY) values(%s,%s,%s)"
-                val = ((f"{a}°-{b}°"),c,d)
-                curs.execute(insert,val)
+        else:
+            break
 
-        elif days_ == "15":
-            print("ტემპერატურა შემდეგი 15 დღის განმავლობაში")    
-            for a,b,c,d in zip(daytemperature,nighttemperature,weekdaycount,daycount):
-                print(f"{d} - {c} - {a}° - {b}°")
-                insert = f"INSERT INTO {city}(WEATHER,WEEKDAY,DAY) values(%s,%s,%s)"
-                val = ((f"{a}°-{b}°"),c,d)
-                curs.execute(insert,val) 
-
-        elif days_ == "25":
-            print("ტემპერატურა შემდეგი 25 დღის განმავლობაში")    
-            for a,b,c,d in zip(daytemperature,nighttemperature,weekdaycount,daycount):
-                print(f"{d} - {c} - {a}° - {b}°")
-                insert = f"INSERT INTO {city}(WEATHER,WEEKDAY,DAY) values(%s,%s,%s)"
-                val = ((f"{a}°-{b}°"),c,d)
-                curs.execute(insert,val) 
 
         mydb.commit()
 
     except Exception as i:
         print(i)
-        retry = input("Error try again?(Yes/No): ")
-        if (retry != "Yes"):
-            break       
-
+        retry = str(input("try again?(Yes/No): "))
+        if retry != "Yes" and retry != "yes":
+            break   
